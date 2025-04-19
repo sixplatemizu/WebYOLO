0c81c1e016a3dc71319a3439bec3a57dd2aa0795
sixplatemizu <53457083+sixplatemizu@users.noreply.github.com>
2025-04-19 15:10:54 +0800

    flutter version problem fixing

--- flutter_ver/flutter_app/windows/runner/main.cpp
+++ flutter_ver/flutter_app/windows/runner/main.cpp
@@ -1,58 +1,21 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
-#include <tlhelp32.h>
 
 #include "flutter_window.h"
 #include "utils.h"
 
-HANDLE backendProcessHandle = nullptr;
-
-bool StartBackendService() {
-    STARTUPINFO si = { sizeof(si) };
-    PROCESS_INFORMATION pi;
-
-    si.dwFlags = STARTF_USESHOWWINDOW;
-    si.wShowWindow = SW_HIDE;
-
-    if (CreateProcess(
-        L"backend\\dist\\main.exe",
-        NULL,
-        NULL,
-        NULL,
-        FALSE,
-        CREATE_NO_WINDOW,
-        NULL,
-        NULL,
-        &si,
-        &pi
-    )) {
-        backendProcessHandle = pi.hProcess;
-        CloseHandle(pi.hThread);
-        return true;
-    }
-    return false;
-}
-
-void StopBackendService() {
-    if (backendProcessHandle) {
-        TerminateProcess(backendProcessHandle, 0);
-        CloseHandle(backendProcessHandle);
-        backendProcessHandle = nullptr;
-    }
-}
-
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-
-  if (!StartBackendService()) {
-    return EXIT_FAILURE;
-  }
 
   flutter::DartProject project(L"data");
 
@@ -65,7 +28,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
   if (!window.Create(L"flutter_yolo_app", origin, size)) {
-    StopBackendService();
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
@@ -75,8 +37,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
-
-  StopBackendService();
 
   ::CoUninitialize();
   return EXIT_SUCCESS;
